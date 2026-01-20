8540b6da5431c609c72de2016af59bb225b2e215
Renako <131531729+renakome@users.noreply.github.com>
2026-01-20 11:21:35 -0300

    Add workflow to build webOS IPK

--- webos/src/js/additional.user.js
+++ webos/src/js/additional.user.js
@@ -3,6 +3,16 @@ if (window.location.pathname.startsWith('/oauth20_authorize.srf')) {
     throw new Error('[Better xCloud] Ignore');
 }
 
+if (window.location.pathname.startsWith('/play/login') && !window.location.pathname.includes('deviceCode')) {
+    window.location = 'https://www.xbox.com/en-US/play/login/deviceCode';
+    throw new Error('[Better xCloud] Ignore');
+}
+
+if (window.location.pathname.startsWith('/login') && !window.location.pathname.includes('deviceCode')) {
+    window.location = 'https://www.xbox.com/en-US/play/login/deviceCode';
+    throw new Error('[Better xCloud] Ignore');
+}
+
 if (!window.location.pathname.includes('/play')) {
     throw new Error('[Better xCloud] Ignore');
 }
